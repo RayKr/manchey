c3fe905cbbed215bf6e6a65cb61a626ff6f03351
raykr <ray_kr@163.com>
2016-05-23 19:47:03 +0800

    struts2注解方式

--- src/main/java/com/manchey/action/RegisterAction.java
+++ src/main/java/com/manchey/action/RegisterAction.java
@@ -1,15 +1,21 @@
 package com.manchey.action;
 
 import com.manchey.service.UserServiceI;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Ray on 2016/5/23.
  */
-@Controller("registerAciton")
+@Component
 @Scope("prototype")
+@Namespace("/test")
+@ParentPackage("struts-default")
 public class RegisterAction {
     private UserServiceI userService;
     private String name;
@@ -36,6 +42,7 @@ public class RegisterAction {
         this.age = age;
     }
 
+    @Action(value = "register", results = @Result(location = "/userinfo.jsp"))
     public String execute() {
         userService.addUser(name, age);
 
