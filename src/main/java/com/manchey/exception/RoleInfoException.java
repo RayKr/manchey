72d8df8df4a6670120e9d80b9809e8b66ddfe58d
raykr <ray_kr@163.com>
2016-05-28 19:46:07 +0800

    异常类

---
+++ src/main/java/com/manchey/exception/RoleInfoException.java
@@ -0,0 +1,14 @@
+package com.manchey.exception;
+
+/**
+ * Created by Ray on 2016/5/28.
+ */
+public class RoleInfoException extends Exception {
+
+    public RoleInfoException() {
+    }
+
+    public RoleInfoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/manchey/exception/SecretLoveException.java
@@ -0,0 +1,8 @@
+package com.manchey.exception;
+
+/**
+ * 暗恋系统异常
+ * Created by Ray on 2016/5/28.
+ */
+public class SecretLoveException extends Exception {
+}
