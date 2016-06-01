ae7d54f543a47b72737015ea6159648504f84df4
raykr <ray_kr@163.com>
2016-06-01 10:25:17 +0800

    验证是否已存在该角色

--- src/main/java/com/manchey/support/impl/CheckRoleServiceImpl.java
+++ src/main/java/com/manchey/support/impl/CheckRoleServiceImpl.java
@@ -3,6 +3,7 @@ package com.manchey.support.impl;
 import com.manchey.dao.BaseDao;
 import com.manchey.exception.RoleInfoException;
 import com.manchey.model.po.TRegion;
+import com.manchey.model.po.TRole;
 import com.manchey.model.po.TServer;
 import com.manchey.model.vo.wechat.RoleInfo;
 import com.manchey.support.CheckRoleService;
@@ -20,6 +21,9 @@ public class CheckRoleServiceImpl implements CheckRoleService {
 
     @Autowired
     private BaseDao<TServer> serverDao;
+
+    @Autowired
+    private BaseDao<TRole> roleDao;
 
     @Override
     public RoleInfo checkRoleCorrect(RoleInfo roleInfo) throws RoleInfoException {
@@ -40,6 +44,13 @@ public class CheckRoleServiceImpl implements CheckRoleService {
         roleInfo.setServerCode(tServer.getServerCode());
 
         // TODO 验证角色，需要讨论具体验证流程
+        hql = "from TRole where serverCode=?0 and roleName=?1 ";
+        TRole tRole = roleDao.get(hql, roleInfo.getServerCode(), roleInfo.getRoleName());
+        if (tRole != null) {
+            // TODO 有个问题，如果其他用户冒用此角色名，如何保证数据的准确性，所以必须有游戏端API或者手工验证。
+            throw new RoleInfoException("该角色名已绑定，无需重复操作，如果信息有误，请联系管理员！");
+        }
+
         return roleInfo;
     }
 }
